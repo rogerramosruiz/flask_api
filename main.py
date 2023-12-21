dad5acb541cb09667ef10e4aead0df86d7db8fea
rgr <rgramos@minsalud.local>
2023-12-21 11:40:55 -0400

    se devuelve solo matricula y titulo

--- main.py
+++ main.py
@@ -13,15 +13,7 @@ CORS(app)
 def get_users(ci):
      with connection() as (cur, _):
         query = """
-        SELECT tp.nombres, 
-        CASE
-            WHEN tp.ap_paterno::text = '--'::text THEN ''::character varying
-            ELSE tp.ap_paterno
-            END AS ap_paterno,
-        CASE
-            WHEN tp.ap_materno::text = '--'::text THEN ''::character varying
-            ELSE tp.ap_materno
-            END AS ap_materno, 
+        SELECT  
             (btrim(f.letmat::text) || ' - '::text) || btrim(f.nromat::text) AS matricula,
             tt.des_titulo,
             tf.des_tipoformacion
@@ -36,12 +28,8 @@ def get_users(ci):
         if data is None:
            return jsonify({'error': 'Cedula invalida'}), 404
         person_data = {
-            'nombres': data[0],
-            'ap_paterno': data[1],
-            'ap_materno': data[2],
-            'matricula': data[3],
-            'titulo': data[4],
-            'formacion': data[5],
+            'matricula': data[0],
+            'titulo': data[1],
         }
         return jsonify(person_data), 200
 
